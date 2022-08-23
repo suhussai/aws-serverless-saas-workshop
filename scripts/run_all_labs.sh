46434aab804c95892afbb31a65c5411b34b21b2f
Syed Hussain <suhussai@ualberta.ca>
2022-08-22 19:13:29 -0600

    update run_all_labs script to help with timing

--- scripts/run_all_labs.sh
+++ scripts/run_all_labs.sh
@@ -4,74 +4,70 @@
 ## using the completed labs found in the Solutions folder.
 ##
 
-echo "################ Running pre-req script... ################"
+echo "$(date) ################ Running pre-req script... ################" | tee -a /tmp/timing-output.log
 cd ../Cloud9Setup/
-./increase-disk-size.sh
+# ./increase-disk-size.sh
 # ./pre-requisites.sh
 cd ../scripts/
-echo "################ Done running pre-req script... ################"
+echo "$(date) ################ Done running pre-req script... ################" | tee -a /tmp/timing-output.log
 
-# echo "################ Running labs... ################"
+echo "$(date) ################ Running labs... ################" | tee -a /tmp/timing-output.log
 
-# #### Note that deploying lab1 is not a requirement ####
-# # echo "################ Running lab1... ################"
-# # cd ../Solution/Lab1/scripts
-# # ./deployment.sh -s -c --stack-name serverless-saas-workshop-lab1
-# # cd ../../../scripts/
-# # echo "################ Done running lab1. ################"
-# #######################################################
+### Note that deploying lab1 is not a requirement ####
+echo "$(date) ################ Running lab1... ################" | tee -a /tmp/timing-output.log
+cd ../Solution/Lab1/scripts
+./deployment.sh -s -c --stack-name serverless-saas-workshop-lab1
+cd ../../../scripts/
+echo "$(date) ################ Done running lab1. ################" | tee -a /tmp/timing-output.log
+######################################################
 
-echo "################ Running lab2... ################"
+echo "$(date) ################ Running lab2... ################" | tee -a /tmp/timing-output.log
 
 cd ../Solution/Lab2/scripts
 ./deployment.sh -s -c --email syeduh+serverlesslab@amazon.com
 ./deployment.sh -s
 cd ../../../scripts/
 
-echo "################ Done running lab2. ################"
+echo "$(date) ################ Done running lab2. ################" | tee -a /tmp/timing-output.log
 
-echo "################ Sleeping for a minute before moving to next lab... ################"
-sleep 60
+read -p "Press enter to continue"
 
-echo "################ Running lab3... ################"
+echo "$(date) ################ Running lab3... ################" | tee -a /tmp/timing-output.log
 
 cd ../Solution/Lab3/scripts
 ./deployment.sh -s -c
 ./deployment.sh -s
 cd ../../../scripts/
 
-echo "################ Done running lab3. ################"
+echo "$(date) ################ Done running lab3. ################" | tee -a /tmp/timing-output.log
 
-echo "################ Sleeping for a minute before moving to next lab... ################"
-sleep 60
+read -p "Press enter to continue"
 
-echo "################ Running lab4... ################"
+echo "$(date) ################ Running lab4... ################" | tee -a /tmp/timing-output.log
 
 cd ../Solution/Lab4/scripts
 ./deployment.sh -s
 cd ../../../scripts/
 
-echo "################ Done running lab4. ################"
+echo "$(date) ################ Done running lab4. ################" | tee -a /tmp/timing-output.log
 
-echo "################ Sleeping for a minute before moving to next lab... ################"
-sleep 60
+read -p "Press enter to continue"
 
-echo "################ Running lab5... ################"
+echo "$(date) ################ Running lab5... ################" | tee -a /tmp/timing-output.log
 
 cd ../Solution/Lab5/scripts/
 ./deployment.sh -s -c
 ./deployment.sh -s
 cd ../../../scripts/
 
-echo "################ Done running lab5. ################"
+echo "$(date) ################ Done running lab5. ################" | tee -a /tmp/timing-output.log
 
-echo "################ Sleeping for a minute before moving to next lab... ################"
-sleep 60
+read -p "Press enter to continue"
 
-echo "################ Running lab6... ################"
+echo "$(date) ################ Running lab6... ################" | tee -a /tmp/timing-output.log
 
 cd ../Solution/Lab6/scripts/
 ./deployment.sh
 cd ../../../scripts/
 
-echo "################ Done running lab6. ################"
+echo "$(date) ################ Done running lab6. ################" | tee -a /tmp/timing-output.log
